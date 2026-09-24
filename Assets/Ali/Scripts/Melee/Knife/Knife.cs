d694eaa2f3e6ad147abafb46dc8922f10765f209
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-09-24 18:04:26 +0300

    save

---
+++ Assets/Ali/Scripts/Melee/Knife/Knife.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Knife : MonoBehaviour
+{
+    [SerializeField] private float timeBetweenAttacks= 0.5f;
+    [SerializeField] private bool canAttack = true;
+    [SerializeField] private float attackDps;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if(context.started && canAttack)
+        {
+            animator.SetTrigger("Attacking");
+        }
+
+    }
+}
